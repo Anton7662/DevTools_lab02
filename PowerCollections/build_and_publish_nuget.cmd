919b96d1aba67057e7e2354513bf6b6956a491db
AntonMarkin <guruzorik@gmail.com>
2023-02-26 18:33:00 +0400

    Add Task01

---
+++ PowerCollections/build_and_publish_nuget.cmd
@@ -0,0 +1,2 @@
+dotnet pack
+dotnet nuget push "PowerCollections\bin\Debug\*.nupkg"  --api-key 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 --source  https://nuget.pkg.github.com/Anton7662/index.json --skip-duplicate
